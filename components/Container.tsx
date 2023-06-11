ff371814c10d4a29c88fee3c7ce204586bb6f18d
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-11 19:38:15 +0800

    create container component

---
+++ components/Container.tsx
@@ -0,0 +1,16 @@
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+const Container = ({ children, className, ...props }: Props) => {
+  return (
+    <main
+      className={"flex-1 container p-4 lg:px-0 mx-auto " + " " + className}
+      {...props}
+    >
+      {children}
+    </main>
+  );
+};
+
+export default Container;
