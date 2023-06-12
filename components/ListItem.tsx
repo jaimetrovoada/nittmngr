d34f7b9b59fa09709a97af60eefcf736e38a5120
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-12 11:09:58 +0800

    modify list item component

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -5,12 +5,9 @@ interface Props {
 
 const ListItem = ({ name, deleteItem }: Props) => {
   return (
-    <li className="list-inside py-4 px-2 border-b border-b-gray-100 hover:bg-gray-50">
+    <li className="list-inside py-4 px-2 border-b border-b-gray-100 hover:bg-gray-50 flex flex-row justify-between items-center">
       {name}
-      <button
-        onClick={() => deleteItem(name)}
-        className="text-red-600 p-2 border border-gray-100"
-      >
+      <button onClick={() => deleteItem(name)} className="text-red-600 p-2">
         x
       </button>
     </li>
