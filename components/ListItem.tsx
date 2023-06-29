3011996148935abb9760fd14e6ca9bbec6926cd2
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-29 12:17:08 +0800

    make each username clickable

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 interface Props {
   name: string;
+  url: string;
   deleteItem: (name: string) => void;
 }
 
-const ListItem = ({ name, deleteItem }: Props) => {
+const ListItem = ({ name, deleteItem, url }: Props) => {
   return (
-    <li className="flex flex-row items-center justify-between border-b border-b-gray-800 p-2 text-sm hover:bg-zinc-900/20 lg:text-base">
-      {name}
-      <button onClick={() => deleteItem(name)} className="p-2 text-red-600">
+    <li className="group flex flex-row items-center justify-between border-b border-b-gray-800 p-2 text-sm hover:bg-zinc-900/20 lg:text-base">
+      <Link href={url} className="underline-offset-4 group-hover:underline">
+        @{name}
+      </Link>
+      <button
+        onClick={() => deleteItem(name)}
+        className="ml-auto p-2 text-red-600"
+      >
         X
       </button>
     </li>
